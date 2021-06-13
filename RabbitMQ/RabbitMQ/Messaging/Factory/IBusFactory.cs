5732284eaa89c90269468f71efafaabbbcd45718
Frank <Frank@DESKTOP-FA8EJCS>
2021-06-13 14:46:13 +0800

    專案初始化

---
+++ RabbitMQ/RabbitMQ/Messaging/Factory/IBusFactory.cs
@@ -0,0 +1,13 @@
+﻿using EasyNetQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Messaging.Factory
+{
+    public interface IBusFactory
+    {
+        IAdvancedBus CrateBus();
+    }
+}
